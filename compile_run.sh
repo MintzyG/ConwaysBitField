d29adf24aeeb2b2f4a369baac80acda6e6f1867d
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-05-17 20:01:38 -0300

    added TTY option in shell menu and default values

--- compile_run.sh
+++ compile_run.sh
@@ -1,20 +1,35 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 echo "How tall do you want the matrix from 1-16:"
 read Height
+Height=${Height:-10}
 
 echo "How wide do you want the matrix from 1-16:"
 read Width
+Width=${Width:-10}
 
 echo "Do you want a random board? (y/Y | n/N)"
 read Rand
+Rand=${Rand:-N}
 
 echo "What delay do you want 1ms - 1000ms:"
 read Delay
+Delay=${Delay:-125}
+
+echo "Are you going to run it through tty? (y/Y | n/N)"
+read Tty
+Tty=${Tty:-Y}
+
+gcc_cmd="gcc main.c -o game.out -DDELAY=$Delay -DWIDTH=$Width -DHEIGHT=$Height -O3 -Wall -Wextra"
 
 if [[ "$Rand" == "Y" || "$Rand" == "y" ]]; then
-  gcc main.c -o game.out -DDELAY=$Delay -DRAND -DWIDTH=$Width -DHEIGHT=$Height -O3 -Wall -Wextra
-else
-  gcc main.c -o game.out -DDELAY=$Delay -DWIDTH=$Width -DHEIGHT=$Height -O3 -Wall -Wextra
+  gcc_cmd+=" -DRAND"
 fi
+
+if [[ "$Tty" == "Y" || "$Tty" == "y" ]]; then
+  gcc_cmd+=" -DTTY"
+fi
+
+eval $gcc_cmd
+
 ./game.out
